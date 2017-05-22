00299fe1b7690e3b172d697e8b24e044e2a19c89
Dinesh Harjani <dinesharjani@gmail.com>
2017-05-22 16:26:30 +0100

    TokenizerMain now supports file arguments
    To test this, we added the mclaren.txt file to the project, and added it as an argument to TokenizerMain with successful results.

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -3,11 +3,44 @@ package org.fogbeam.example.opennlp;
 
 import org.fogbeam.example.opennlp.dinesh.StringTokenizer;
 
+import java.io.*;
+
 public class TokenizerMain {
-	public static void main( String[] args ) throws Exception {
-		
-		StringTokenizer.tokenize( "A ranger journeying with Oglethorpe, founder of the Georgia Colony, "
-				+ " mentions \"three Mounts raised by the Indians over three of their Great Kings"
-				+ " who were killed in the Wars.\"");
+
+    public static void main( String[] args ) throws Exception {
+
+	    if (args.length > 0) {
+	        for (final String filePath : args) {
+	            StringTokenizer.tokenize(readStringFromFile(filePath));
+            }
+        } else {
+	        // Demo mode.
+            StringTokenizer.tokenize( "A ranger journeying with Oglethorpe, founder of the Georgia Colony, "
+                    + " mentions \"three Mounts raised by the Indians over three of their Great Kings"
+                    + " who were killed in the Wars.\"");
+        }
 	}
+
+	private static String readStringFromFile(final String filePath) {
+
+        String fileContents = "";
+        try {
+            final InputStream is = new FileInputStream(filePath);
+            final BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
+
+            String line = buffer.readLine();
+            final StringBuilder stringBuilder = new StringBuilder();
+            while (line != null) {
+                stringBuilder.append(line).append("\n");
+                line = buffer.readLine();
+            }
+            fileContents = stringBuilder.toString();
+        } catch (final FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (final IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return fileContents;
+    }
 }
